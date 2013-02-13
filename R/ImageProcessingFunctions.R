fed9c94800f8601926f84a167ed0b37be55d0b93
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-02-13 23:07:58 +0000

    fix binarize argument to cmtk levelset command

--- R/ImageProcessingFunctions.R
+++ R/ImageProcessingFunctions.R
@@ -1,4 +1,4 @@
-CMTKLevelset<-function(fin,fout,args='--binary'){
+CMTKLevelset<-function(fin,fout,args='--binarize'){
 	cmd=paste("levelset",args,shQuote(fin),shQuote(fout))
 	rval=system(cmd)
 	if(rval!=0) stop("Levelset failed")
