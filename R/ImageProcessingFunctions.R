8b3034f807fafa522f1939f441fffd24446f45c2
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-02-13 23:13:33 +0000

    RunCmdForNewerInput returns F when it can't do anything AND when it won't do anything

--- R/ImageProcessingFunctions.R
+++ R/ImageProcessingFunctions.R
@@ -1,6 +1,5 @@
 CMTKLevelset<-function(fin,fout,args='--binarize',...){
 	cmd=paste("levelset",args,shQuote(fin),shQuote(fout))
-	success=RunCmdForNewerInput(cmd,fin,fout,...)
-	if(!success) stop("Levelset failed")
+	something_happened=RunCmdForNewerInput(cmd,fin,fout,...)
 	return(fout)
 }
