691c5f7be6631189547efb75302ba1bccdb35ae0
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-02-15 21:06:50 +0000

    added D virilis and D melanogaster
    note: D melanogaster looks small.

--- R/DownsampledTemplates.R
+++ R/DownsampledTemplates.R
@@ -1,3 +1,8 @@
+#asymmetric template brains
+
+# Dsimulans
+###########
+
 leaconfig$Dsim$IS1=file.path(leaconfig$rootdir,"Dsimulans",'DsimDBM/refbrain/DsimIS1.nrrd')
 leaconfig$Dsim$IS1.2um=file.path(leaconfig$rootdir,"Dsimulans",'DsimDBM/refbrain/DsimIS1_2um.nrrd')
 
@@ -7,4 +12,31 @@ NrrdResample(leaconfig$Dsim$IS1,leaconfig$Dsim$IS1.2um,voxdims=c(2,2,2))
 
 leaconfig$Dsim$IS1.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dsim$IS1.2um)
 CMTKLevelset(leaconfig$Dsim$IS1.2um,leaconfig$Dsim$IS1.2um.mask)
+
+# D virilis
+###########
+leaconfig$Dvir$IS1=file.path(leaconfig$rootdir,"Dvirilis",'DvirDBM/refbrain/DvirIS1.nrrd')
+leaconfig$Dvir$IS1.2um=file.path(leaconfig$rootdir,"Dvirilis",'DvirDBM/refbrain/DvirIS1_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dvir$IS1,leaconfig$Dvir$IS1.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dvir$IS1.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dvir$IS1.2um)
+CMTKLevelset(leaconfig$Dvir$IS1.2um,leaconfig$Dvir$IS1.2um.mask)
+
+# D melanogaster
+###########
+
+leaconfig$Dmel$IS1=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelIS/FBIS1.nrrd')
+leaconfig$Dmel$IS1.2um=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelIS/FBIS1_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dmel$IS1,leaconfig$Dmel$IS1.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dmel$IS1.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dmel$IS1.2um)
+CMTKLevelset(leaconfig$Dmel$IS1.2um,leaconfig$Dmel$IS1.2um.mask)
+
+
 
