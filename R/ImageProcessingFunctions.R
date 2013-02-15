aed13d952440514c1c1ffcda8cdfe947622ebade
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-02-15 22:05:01 +0000

    add function to calculate jacobians

--- R/ImageProcessingFunctions.R
+++ R/ImageProcessingFunctions.R
@@ -3,3 +3,24 @@ CMTKLevelset<-function(fin,fout,args='--binarize',...){
 	something_happened=RunCmdForNewerInput(cmd,fin,fout,...)
 	return(fout)
 }
+
+#' Calculate Jacobian determinants for a given registration
+#'
+#' If fout refers to a directory then a default output filename
+#' will be generated. e.g.:
+#' if reg is /path/to/some/regs/myreg.list
+#' and fout is /some/other/path/jacs
+#' /some/other/path/jacs/myreg_jac.nrrd
+CMTKJacobian<-function(target,reg,fout,args='',targetIsMask=TRUE,...){
+	#if(missing(fout)) fout=sub("\\.list$","_jac.nrrd",basename(reg))
+	if(length(fout)==1 && file.info(fout)$isdir){
+		# if reg is /path/to/some/regs/myreg.list
+		# and fout is /some/other/path/jacs
+		# /some/other/path/jacs/myreg_jac.nrrd
+		fout=file.path(fout,sub("\\.list$","_jac.nrrd",basename(reg)))
+	}
+	if(targetIsMask) args=paste(args,"--mask")
+	cmd=paste("reformatx","--outfile",shQuote(fout),args,shQuote(target), "--jacobian",shQuote(reg))
+	something_happened=RunCmdForNewerInput(cmd,c(reg,target),fout,...)
+	return(fout)
+}
