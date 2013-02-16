12d4615f59c70baee89211fb33e20810732fcefd
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-02-16 00:22:15 +0000

    Add function to calculate t stats with CMTK (from Jacobians)

--- R/ImageProcessingFunctions.R
+++ R/ImageProcessingFunctions.R
@@ -4,6 +4,7 @@ CMTKLevelset<-function(fin,fout,args='--binarize',...){
 	return(fout)
 }
 
+
 #' Calculate Jacobian determinants for a given registration
 #'
 #' If fout refers to a directory then a default output filename
@@ -23,4 +24,24 @@ CMTKJacobian<-function(target,reg,fout,args='',targetIsMask=TRUE,...){
 	cmd=paste("reformatx","--outfile",shQuote(fout),args,shQuote(target), "--jacobian",shQuote(reg))
 	something_happened=RunCmdForNewerInput(cmd,c(reg,target),fout,...)
 	return(fout)
+}
+#' Compute t test for two groups of jacobian determinants
+#'
+#' note it is nearly always a good idea to log transform jacobians
+CMTKTtest<-function(group1,group2,out.stem,mask=NULL,log=TRUE,args='',...){
+	if(log) args=paste(args,'--log')
+	if(!is.null(mask)) args=paste(args,"--mask",shQuote(mask))
+	
+	pgroup1=paste(shQuote(group1),collapse=" ")
+	pgroup2=paste(shQuote(group2),collapse=" ")
+
+	# p values and t statistics
+	out.pvals=paste(out.stem,sep="-",'pvals.nrrd')
+	out.tvals=paste(out.stem,sep="-",'tvals.nrrd')
+	
+	cmd=paste("ttest",args,"--outfile",shQuote(out.pvals),
+		"--tstats-file",shQuote(out.tvals),
+		pgroup1,"--",pgroup2)
+	something_happened=RunCmdForNewerInput(cmd,c(group1,group2,mask),c(out.tvals,out.pvals),...)
+	return(c(out.pvals=out.pvals,out.tvals=out.tvals))
 }
