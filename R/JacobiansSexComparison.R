7385452271a6eece08e3c8735111db68cb2cd8f4
lgoetz <lg399@cam.ac.uk>
2013-02-28 19:49:10 +0000

    added Dmelanogaster and Dyakuba commands (though won't work yet since for Dmel the registrations and for Dyak everything is missing)

--- R/JacobiansSexComparison.R
+++ R/JacobiansSexComparison.R
@@ -32,3 +32,35 @@ if(!file.exists(leaconfig$Dvir$IS1.jacs))
 for(thisreg in sample(dir(leaconfig$Dvir$IS1.reg,patt='\\.list$',full=T))){
 	CMTKJacobian(target=leaconfig$Dvir$IS1.2um.mask,reg=thisreg,fout=leaconfig$Dvir$IS1.jacs,UseLock=TRUE)
 }
+
+# D melanogaster
+###########
+
+leaconfig$Dmel$IS1.reg=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelDBM/Registration/warp')
+leaconfig$Dmel$IS1.jacs=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelDBM/jacs')
+
+if(!file.exists(leaconfig$Dmel$IS1.jacs))
+	dir.create(leaconfig$Dmel$IS1.jacs)
+
+# nb dir will only return things that end in .list
+# nb sample randomises order of registration directories
+# to avoid competition when multiple jobs are running this script
+for(thisreg in sample(dir(leaconfig$Dmel$IS1.reg,patt='\\.list$',full=T))){
+	CMTKJacobian(target=leaconfig$Dvir$IS1.2um.mask,reg=thisreg,fout=leaconfig$Dvir$IS1.jacs,UseLock=TRUE)
+}
+
+# Dyakuba - can't use yet since files don't exist yet 
+###########
+
+leaconfig$Dyak$IS1.reg=file.path(leaconfig$rootdir,"Dyakuba",'DyakDBM/Registration/warp')
+leaconfig$Dyak$IS1.jacs=file.path(leaconfig$rootdir,"Dyakuba",'DyakDBM/jacs')
+if(!file.exists(leaconfig$Dyak$IS1.jacs))
+	dir.create(leaconfig$Dyak$IS1.jacs)
+
+# nb dir will only return things that end in .list
+# nb sample randomises order of registration directories
+# to avoid competition when multiple jobs are running this script
+for(thisreg in sample(dir(leaconfig$Dyak$IS1.reg,patt='\\.list$',full=T))){
+	CMTKJacobian(target=leaconfig$Dyak$IS1.2um.mask,reg=thisreg,fout=leaconfig$Dyak$IS1.jacs,UseLock=TRUE)
+}
+
