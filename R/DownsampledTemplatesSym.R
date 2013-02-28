0eb95a69eea7d2ba178b6218fedf642c104e7ea1
lgoetz <lg399@cam.ac.uk>
2013-02-28 22:06:34 +0000

    calculate the jacobian determinants to compare the two hemispheres

---
+++ R/DownsampledTemplatesSym.R
@@ -0,0 +1,28 @@
+#asymmetric template brains
+
+# Dsimulans
+###########
+
+# for female
+
+leaconfig$Dsim$Fsym=file.path(leaconfig$rootdir,"Dsimulans",'DsimFsym/refbrain/Dsimf-1.nrrd')
+leaconfig$Dsim$Fsym.2um=file.path(leaconfig$rootdir,"Dsimulans",'DsimFsym/refbrain/DsimFsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dsim$Fsym,leaconfig$Dsim$Fsym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dsim$Fsym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dsim$Fsym.2um)
+CMTKLevelset(leaconfig$Dsim$Fsym.2um,leaconfig$Dsim$Fsym.2um.mask)
+
+# for male
+
+leaconfig$Dsim$Msym=file.path(leaconfig$rootdir,"Dsimulans",'DsimMsym/refbrain/DsimMsym.nrrd')
+leaconfig$Dsim$Msym.2um=file.path(leaconfig$rootdir,"Dsimulans",'DsimMsym/refbrain/DsimMsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dsim$Msym,leaconfig$Dsim$Msym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dsim$Msym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dsim$Msym.2um)
+CMTKLevelset(leaconfig$Dsim$Msym.2um,leaconfig$Dsim$Msym.2um.mask)
