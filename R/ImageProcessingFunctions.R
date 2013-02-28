b9a77daa536743dbdcadbe76b35ecdda76bd73c7
lgoetz <lg399@cam.ac.uk>
2013-02-28 19:58:58 +0000

    fix the omission of "…" after changing the threshold in CMTKLevelset (was committed earlier under the wrong message)

--- R/ImageProcessingFunctions.R
+++ R/ImageProcessingFunctions.R
@@ -1,4 +1,4 @@
-CMTKLevelset<-function(fin,fout,args='--binarize --levelset-threshold 2'){
+CMTKLevelset<-function(fin,fout,args='--binarize --levelset-threshold 2',...){
 	cmd=paste("levelset",args,shQuote(fin),shQuote(fout))
 	something_happened=RunCmdForNewerInput(cmd,fin,fout,...)
 	return(fout)
