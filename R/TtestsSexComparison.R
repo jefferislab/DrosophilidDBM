68173d87c0300bfc6c8f96db98d6fa1b9e5b4eec
lgoetz <lg399@cam.ac.uk>
2013-03-03 23:07:17 +0000

    duplicated the Dsimulans & Dvirlis commands for the T-tests to do the sex comparison for Dmelanogaster and Dyakuba.

--- R/TtestsSexComparison.R
+++ R/TtestsSexComparison.R
@@ -50,3 +50,55 @@ CMTKTtest(leaconfig$Dvir$IS1.jacfiles[leaconfig$Dvir$IS1.jacfiles.sex=='M'],
 	mask=leaconfig$Dvir$IS1.2um.mask
 )
 
+# Dmelanogaster
+###########
+
+leaconfig$Dmel$IS1.stats=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelDBM/stats')
+if(!file.exists(leaconfig$Dmel$IS1.stats))
+	dir.create(leaconfig$Dmel$IS1.stats)
+
+# load spreadsheet with sexes
+
+leaconfig$Dmel$brains=read.table(file.path(leaconfig$rootdir,"db",'DmelBrains.txt'),header=T)
+rownames(leaconfig$Dmel$brains)=leaconfig$Dmel$brains$brain
+
+# list the full path to jacobians
+leaconfig$Dmel$IS1.jacfiles=dir(leaconfig$Dmel$IS1.jacs,full=T)
+# figure out sex for each one
+leaconfig$Dmel$IS1.jacfiles.sex=leaconfig$Dmel$brains[brain_from_path(leaconfig$Dmel$IS1.jacfiles),'sex']
+
+# Barf if we couldn't figure out the sex of one of the brains
+stopifnot(all(!is.na(leaconfig$Dmel$IS1.jacfiles.sex)))
+
+CMTKTtest(leaconfig$Dmel$IS1.jacfiles[leaconfig$Dmel$IS1.jacfiles.sex=='M'],
+	leaconfig$Dmel$IS1.jacfiles[leaconfig$Dmel$IS1.jacfiles.sex=='F'],
+	out.stem=file.path(leaconfig$Dmel$IS1.stats,'MF'),
+	mask=leaconfig$Dmel$IS1.2um.mask
+)
+
+# Dyakuba
+###########
+
+leaconfig$Dyak$IS1.stats=file.path(leaconfig$rootdir,"Dyakuba",'DyakDBM/stats')
+if(!file.exists(leaconfig$Dyak$IS1.stats))
+	dir.create(leaconfig$Dyak$IS1.stats)
+
+# load spreadsheet with sexes
+
+leaconfig$Dyak$brains=read.table(file.path(leaconfig$rootdir,"db",'DyakBrains.txt'),header=T)
+rownames(leaconfig$Dyak$brains)=leaconfig$Dyak$brains$brain
+
+# list the full path to jacobians
+leaconfig$Dyak$IS1.jacfiles=dir(leaconfig$Dyak$IS1.jacs,full=T)
+# figure out sex for each one
+leaconfig$Dyak$IS1.jacfiles.sex=leaconfig$Dyak$brains[brain_from_path(leaconfig$Dyak$IS1.jacfiles),'sex']
+
+# Barf if we couldn't figure out the sex of one of the brains
+stopifnot(all(!is.na(leaconfig$Dyak$IS1.jacfiles.sex)))
+
+CMTKTtest(leaconfig$Dyak$IS1.jacfiles[leaconfig$Dyak$IS1.jacfiles.sex=='M'],
+	leaconfig$Dyak$IS1.jacfiles[leaconfig$Dyak$IS1.jacfiles.sex=='F'],
+	out.stem=file.path(leaconfig$Dyak$IS1.stats,'MF'),
+	mask=leaconfig$Dyak$IS1.2um.mask
+)
+
