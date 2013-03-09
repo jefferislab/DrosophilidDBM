c6f905506445f9e756176c82c4b772f468e9c1b8
lgoetz <lg399@cam.ac.uk>
2013-03-09 20:23:53 +0000

    duplicated the Dsim commands for Dvir, Dmel and Dyak

--- R/DownsampledTemplatesSym.R
+++ R/DownsampledTemplatesSym.R
@@ -26,3 +26,84 @@ NrrdResample(leaconfig$Dsim$Msym,leaconfig$Dsim$Msym.2um,voxdims=c(2,2,2))
 
 leaconfig$Dsim$Msym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dsim$Msym.2um)
 CMTKLevelset(leaconfig$Dsim$Msym.2um,leaconfig$Dsim$Msym.2um.mask)
+
+# Dvirilis
+###########
+
+# for female
+
+leaconfig$Dvir$Fsym=file.path(leaconfig$rootdir,"Dvirilis",'DvirFsym/refbrain/Dvirf-1.nrrd')
+leaconfig$Dvir$Fsym.2um=file.path(leaconfig$rootdir,"Dvirilis",'DvirFsym/refbrain/DvirFsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dvir$Fsym,leaconfig$Dvir$Fsym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dvir$Fsym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dvir$Fsym.2um)
+CMTKLevelset(leaconfig$Dvir$Fsym.2um,leaconfig$Dvir$Fsym.2um.mask)
+
+# for male
+
+leaconfig$Dvir$Msym=file.path(leaconfig$rootdir,"Dvirilis",'DvirMsym/refbrain/DvirMsym.nrrd')
+leaconfig$Dvir$Msym.2um=file.path(leaconfig$rootdir,"Dvirilis",'DvirMsym/refbrain/DvirMsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dvir$Msym,leaconfig$Dvir$Msym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dvir$Msym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dvir$Msym.2um)
+CMTKLevelset(leaconfig$Dvir$Msym.2um,leaconfig$Dvir$Msym.2um.mask)
+
+# Dmelanogaster
+###########
+
+# for female
+
+leaconfig$Dmel$Fsym=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelFsym/refbrain/Dmelf-1.nrrd')
+leaconfig$Dmel$Fsym.2um=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelFsym/refbrain/DmelFsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dmel$Fsym,leaconfig$Dmel$Fsym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dmel$Fsym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dmel$Fsym.2um)
+CMTKLevelset(leaconfig$Dmel$Fsym.2um,leaconfig$Dmel$Fsym.2um.mask)
+
+# for male
+
+leaconfig$Dmel$Msym=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelMsym/refbrain/DmelMsym.nrrd')
+leaconfig$Dmel$Msym.2um=file.path(leaconfig$rootdir,"Dmelanogaster",'DmelMsym/refbrain/DmelMsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dmel$Msym,leaconfig$Dmel$Msym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dmel$Msym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dmel$Msym.2um)
+CMTKLevelset(leaconfig$Dmel$Msym.2um,leaconfig$Dmel$Msym.2um.mask)
+
+# Dyakuba
+###########
+
+# for female
+
+leaconfig$Dyak$Fsym=file.path(leaconfig$rootdir,"Dyakuba",'DyakFsym/refbrain/Dyakf-1.nrrd')
+leaconfig$Dyak$Fsym.2um=file.path(leaconfig$rootdir,"Dyakuba",'DyakFsym/refbrain/DyakFsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dyak$Fsym,leaconfig$Dyak$Fsym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dyak$Fsym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dyak$Fsym.2um)
+CMTKLevelset(leaconfig$Dyak$Fsym.2um,leaconfig$Dyak$Fsym.2um.mask)
+
+# for male
+
+leaconfig$Dyak$Msym=file.path(leaconfig$rootdir,"Dyakuba",'DyakMsym/refbrain/DyakMsym.nrrd')
+leaconfig$Dyak$Msym.2um=file.path(leaconfig$rootdir,"Dyakuba",'DyakMsym/refbrain/DyakMsym_2um.nrrd')
+
+# make a downsampled template brain
+NrrdResample(leaconfig$Dyak$Msym,leaconfig$Dyak$Msym.2um,voxdims=c(2,2,2))
+# use cmtk levelset tool to make a binary mask
+
+leaconfig$Dyak$Msym.2um.mask=sub(".nrrd","_mask.nrrd",leaconfig$Dyak$Msym.2um)
+CMTKLevelset(leaconfig$Dyak$Msym.2um,leaconfig$Dyak$Msym.2um.mask)
