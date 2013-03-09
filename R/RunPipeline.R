785402045a2da9972bc3b00e60853370634006e1
lgoetz <lg399@cam.ac.uk>
2013-03-09 20:24:19 +0000

    added the hemisphere-comparison scripts to the pipeline

--- R/RunPipeline.R
+++ R/RunPipeline.R
@@ -1,5 +1,14 @@
 source("LeaStartup.R", chdir = TRUE)
 setwd(file.path(leaconfig$rootdir,"R"))
+
+# for sex-comparison using asymmetric template
 source("DownsampledTemplates.R", chdir = TRUE)
 source("JacobiansSexComparison.R", chdir = TRUE)
 source("TtestsSexComparison.R", chdir = TRUE)
+
+# for hemisphere-comparison using symmetric template
+source("DownsampledTemplatesSym.R", chdir = TRUE)
+source("JacobiansHemisphereComparison.R", chdir = TRUE)
+source("TtestsHemisphereComparison.R", chdir = TRUE)
+
+
