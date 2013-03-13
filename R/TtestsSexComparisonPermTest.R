dc4425bb0745c2149dfb163d4a50ea8421be4722
lgoetz <lg399@cam.ac.uk>
2013-03-13 18:32:10 +0000

    Fix typo in perm test script & change to 1000 its

--- R/TtestsSexComparisonPermTest.R
+++ R/TtestsSexComparisonPermTest.R
@@ -6,10 +6,10 @@
 
 outfile=file.path(leaconfig$datadir,'DsimSexPerms.rda')
 
-if(RunCmdForNewerInput(NULL,leaconfig$Dsim$IS1.jacfiles,outfile,...)){
+if(RunCmdForNewerInput(NULL,leaconfig$Dsim$IS1.jacfiles,outfile)){
 	# we have some work to do
 	set.seed(42)
-	results=replicate(2,CMTKTtest.Perm(leaconfig$Dsim$IS1.jacfiles[leaconfig$Dsim$IS1.jacfiles.sex=='M'],
+	results=replicate(1000,CMTKTtest.Perm(leaconfig$Dsim$IS1.jacfiles[leaconfig$Dsim$IS1.jacfiles.sex=='M'],
 		leaconfig$Dsim$IS1.jacfiles[leaconfig$Dsim$IS1.jacfiles.sex=='F'],
 		mask=leaconfig$Dsim$IS1.2um.mask))
 	save(results,file=outfile)
