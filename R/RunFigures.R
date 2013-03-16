760c3a692e971b135f3e96a1c7c50d87b5e76fa9
lgoetz <lg399@cam.ac.uk>
2013-03-16 12:14:06 +0000

    split from RunPipeline: run Figures

---
+++ R/RunFigures.R
@@ -0,0 +1,6 @@
+# generating heatmaps showing Male-enlarged/Female-enlarged regions
+source("figs/DsimSexComparison.R", chdir = TRUE)
+
+# generating heatmaps showing Left-enlarged regions
+# using same t-value range for both males and females
+source("figs/HemisphereHeatMapsScaled.R", chdir = TRUE)
