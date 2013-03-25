554a4912d36318283c0a80a7f73396182eeea2ed
Gregory Jefferis <jefferis@gmail.com>
2013-03-25 00:22:18 +0000

    oneway.permtest() now has option to return p value only

--- R/StatsFunctions.R
+++ R/StatsFunctions.R
@@ -7,13 +7,14 @@
 #' @param x, y Values for two groups
 #' @param group.x, group.y Character vectors specifying group names
 #' @param nperms Number of permutations (default 10000)
-#' @seealso t.test, wilcox.test, oneway_test
+#' @param pvalOnly Return only p value (default FALSE prints nice looking result)
+#' @seealso t.test, wilcox.test, oneway_test, coin
 #' @examples
 #' Dsim_MB=rnorm(30,mean=300,10)
 #' Dmel_MB=rnorm(20,mean=350,20)
 #' oneway.permtest(Dsim_MB,Dmel_MB,'Dsim','Dmel')
 #' oneway.permtest(Dsim_MB,Dmel_MB,'Dsim','Dmel')
-oneway.permtest<-function(x,y,group.x="X",group.y="Y",nperms=10000){
+oneway.permtest<-function(x,y,group.x="X",group.y="Y",nperms=10000,pvalOnly=FALSE){
   # coin library implements what we need
   installnload("coin")
   groupnames=c(group.x,group.y)
@@ -22,6 +23,9 @@ oneway.permtest<-function(x,y,group.x="X",group.y="Y",nperms=10000){
   # are not sorted alphabetically 
   # (which may cause surprises if the test is dobe for A,B when we were given B,A)
   regiondf=data.frame(vol=c(x,y),group=factor(group,levels=groupnames))  
-  oneway_test(vol~group,data=regiondf,distribution=approximate(B=nperms))
+  res=oneway_test(vol~group,data=regiondf,distribution=approximate(B=nperms))
+  # return p value if this is all we want
+  if(pvalOnly) pvalue(vr)
+  else res
 }
 
