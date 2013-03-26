9f21909e1521b48f9d1308147c062474036ce01e
lgoetz <lg399@cam.ac.uk>
2013-03-26 14:09:41 +0000

    comment out irrelevant code, clean up vstats

--- R/NeuropilVolumes.R
+++ R/NeuropilVolumes.R
@@ -19,18 +19,22 @@ leaconfig$Dvir$FBIS1bridging.jacs=file.path(leaconfig$rootdir,"FBIS1_Bridging",'
 if(!file.exists(leaconfig$Dvir$FBIS1bridging.jacs))
 	dir.create(leaconfig$Dvir$FBIS1bridging.jacs)
 
+# this maps the Dvirilis reference brain onto the Dmelanogaster FBIS1 template
 CMTKJacobian(target=leaconfig$Dmel$IS1.2um.mask,reg=leaconfig$Dvir$FBIS1bridging,
 	fout=leaconfig$Dvir$FBIS1bridging.jacs,UseLock=TRUE)
 	
-vstats=NeuropilStats(dir(leaconfig$Dvir$FBIS1bridging.jacs,full=TRUE),mask=leaconfig$Dmel$IS1.2um.neuropilmask)
+vstats=NeuropilStats(dir(leaconfig$Dvir$FBIS1bridging.jacs,full=TRUE,patt='\\.nrrd$'),
+	mask=leaconfig$Dmel$IS1.2um.neuropilmask,masklabels=jfrcmaterials)
 
-# drop first row (exterior), 8um^3 is voxel volume
+# drop first row (exterior), 
+# gives the volume of all the non-zero voxels (all voxels*8 since 8um^3 is voxel volume)
 FBIS1.npvol=sum(vstats$n[-1])*8
+# sum calculates the volume of Dvir template (by multiplying each voxel w/ average jacobian det)
 DvirIS1.npvol=sum(vstats$sum[-1])*8
 # e.g. test left lobula in male and female melanogaster
 # but not so interesting really since the DBM lets us do this voxel by voxel!
-LO_L.prop=sapply(npvols$Dmel$IS1,function(df) df['LO_L','sum']/sum(df[-1,'sum']))
-t.test(LO_L.prop~leaconfig$Dmel$IS1.jacfiles.sex)
+# LO_L.prop=sapply(npvols$Dmel$IS1,function(df) df['LO_L','sum']/sum(df[-1,'sum']))
+# t.test(LO_L.prop~leaconfig$Dmel$IS1.jacfiles.sex)
 
 ###########
 # Generate jacobians for Dvir in FBIS1 space
@@ -52,7 +56,7 @@ for(thisreg in sample(dir(leaconfig$Dvir$IS1.reg,patt='\\.list$',full=T))){
 # We can now compare the abs or relative (preferred) neuropil volumes for 
 # Dvir vs Dmel
 
-leaconfig$Dvir$DmelIS1.jacfiles=dir(leaconfig$Dvir$DmelIS1.jacs,full=T)
+leaconfig$Dvir$DmelIS1.jacfiles=dir(leaconfig$Dvir$DmelIS1.jacs,full=T,patt='\\.nrrd$')
 
 # first calculate per neuropil region statistics (e.g. average jacobian)
 npvols$Dvir$DmelIS1=lapply(leaconfig$Dvir$DmelIS1.jacfiles,NeuropilStats,
@@ -61,8 +65,8 @@ npvols$Dvir$DmelIS1=lapply(leaconfig$Dvir$DmelIS1.jacfiles,NeuropilStats,
 
 # just one example:
 # calculate proportion of total neuropil occupied by Left lobula
-Dvir.LO_L.prop=sapply(npvols$Dvir$DmelIS1,function(df) df['LO_L','sum']/sum(df[-1,'sum']))
-t.test(LO_L.prop,Dvir.LO_L.prop)
+# Dvir.LO_L.prop=sapply(npvols$Dvir$DmelIS1,function(df) df['LO_L','sum']/sum(df[-1,'sum']))
+# t.test(LO_L.prop,Dvir.LO_L.prop)
 
 # NB we don't want to look at exterior and LOP_R is missing from the mask
 for(np_region in setdiff(names(jfrcmaterials),c("Exterior",'LOP_R'))){
@@ -82,7 +86,8 @@ if(!file.exists(leaconfig$Dsim$FBIS1bridging.jacs))
 CMTKJacobian(target=leaconfig$Dmel$IS1.2um.mask,reg=leaconfig$Dsim$FBIS1bridging,
 	fout=leaconfig$Dsim$FBIS1bridging.jacs,UseLock=TRUE)
 	
-vstats=NeuropilStats(dir(leaconfig$Dsim$FBIS1bridging.jacs,full=TRUE),mask=leaconfig$Dmel$IS1.2um.neuropilmask)
+vstats=NeuropilStats(dir(leaconfig$Dsim$FBIS1bridging.jacs,full=TRUE,patt='\\.nrrd$'),
+	mask=leaconfig$Dmel$IS1.2um.neuropilmask,masklabels=jfrcmaterials)
 
 # drop first row (exterior), 8um^3 is voxel volume
 FBIS1.npvol=sum(vstats$n[-1])*8
@@ -112,7 +117,7 @@ for(thisreg in sample(dir(leaconfig$Dsim$IS1.reg,patt='\\.list$',full=T))){
 # We can now compare the abs or relative (preferred) neuropil volumes for 
 # Dsim vs Dmel
 
-leaconfig$Dsim$DmelIS1.jacfiles=dir(leaconfig$Dsim$DmelIS1.jacs,full=T)
+leaconfig$Dsim$DmelIS1.jacfiles=dir(leaconfig$Dsim$DmelIS1.jacs,full=T,patt='\\.nrrd$')
 
 # first calculate per neuropil region statistics (e.g. average jacobian)
 npvols$Dsim$DmelIS1=lapply(leaconfig$Dsim$DmelIS1.jacfiles,NeuropilStats,
