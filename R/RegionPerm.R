d94c2b4b014f73201c47fdc6890bbf485a4c8412
lgoetz <lg399@cam.ac.uk>
2013-03-27 15:19:25 +0000

    RegionPerm does now what it is supposed to do: it calculates fractional volumes for each np_region for each species. it further calculates whether these fractional volumes differ (nb: could be both rel greater or smaller) from those in Dmel. and it calculates their absolute volumes (i.e. rel*8).

--- R/RegionPerm.R
+++ R/RegionPerm.R
@@ -1,33 +1,57 @@
-# note: this script does not make much sense yet. 
-###############
-
 # NB Pipeline must be run first to set up variables
 # source("RunPipeline.R", chdir = TRUE)
 # source("NeuropilVolumes.R first, chdir = TRUE")
 
-# Dsimulans
+# Dvirilis
 ###########
 
-# if pipeline and NeuropilVolumes.R is run this should be set up:
-# compute the region statistics for all Dsim and Dmel sample brains compared to JRFC2_template
-# Dsim=sapply(npvols$Dsim$DmelIS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
-# Dmel=sapply(npvols$Dmel$IS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
+# pipeline and NeuropilVolumes.R should be run then this should be set up.
 
-# get region statistics from all the sample brains
-allvals=c(Dsim,Dmel)
-# randomly permute allvals
-fakeIdxs=sample(length(allvals))
-fakeDsimIdxs=sample(length(allvals),size=length(Dsim))
+npstats=list()
+npstats$DvirDmel=list()
+sorted_region_names=sort(names(jfrcmaterials)[-1])
 
+# make skeleton data frame to hold fractional volumes
+fractional_volumes=expand.grid(species=c("Dmel",'Dsim','Dvir','Dyak'),region=sorted_region_names)
+fractional_volumes$fvol=NA
+rownames(fractional_volumes)=paste(fractional_volumes$species,sep="-",fractional_volumes$region)
+for(np_region in sorted_region_names){
+	for(species in c("Dvir","Dsim","Dmel")){
+		statstables=if(species=="Dmel") npvols$Dmel$IS1 else npvols[[species]]$DmelIS1
+		fvol=mean(sapply(statstables,function(df) df[np_region,'sum']/sum(df[-1,'sum'])))
+		fractional_volumes[paste(species,sep="-",np_region),'fvol']=fvol
+	}
+}
 
-outfile=file.path(leaconfig$datadir,'DsimRegionPerms.rda')
+# now calculate stats vs Dmel
+for(np_region in sorted_region_names){
+	Dvir=sapply(npvols$Dvir$DmelIS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
+	Dmel=sapply(npvols$Dmel$IS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
+	npstats$DvirDmel[[np_region]]=oneway.permtest(Dvir,Dmel)
+}
 
-if(RunCmdForNewerInput(NULL,leaconfig$datadir,outfile)){
-	# we have some work to do
-	set.seed(42)
-	results=replicate(1000,CMTKTtest.Perm(fakeDsimIdxs,fakeDmelIdxs,
-		mask=leaconfig$Dmel$IS1.2um.neuropilmask))
-	save(results,file=outfile)
+abs_volumes=expand.grid(species=c("Dmel",'Dsim','Dvir'),region=sorted_region_names)
+abs_volumes$vol=NA
+rownames(abs_volumes)=paste(abs_volumes$species,sep="-",abs_volumes$region)
+for(np_region in sorted_region_names){
+	for(species in c("Dvir","Dsim","Dmel")){
+		statstables=if(species=="Dmel") npvols$Dmel$IS1 else npvols[[species]]$DmelIS1
+		vol=mean(sapply(statstables,function(df) df[np_region,'sum']*8))
+		abs_volumes[paste(species,sep="-",np_region),'vol']=vol
+	}
+}
+
+# Dsimulans
+###########
+ 
+# we don't have to calculate the relative and absolute volumes again
+# since we already did that above in the Dvir script 
+npstats$DsimDmel=list()
+# now calculate stats vs Dmel
+for(np_region in sorted_region_names){
+	Dsim=sapply(npvols$Dsim$DmelIS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
+	Dmel=sapply(npvols$Dmel$IS1,function(df) df[np_region,'sum']/sum(df[-1,'sum']))
+	npstats$DsimDmel[[np_region]]=oneway.permtest(Dsim,Dmel)
 }
 
 
