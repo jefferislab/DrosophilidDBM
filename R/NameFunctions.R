d2aed6c34a547b3757c1ff54b54e24ce306cce05
Gregory Jefferis <jefferis@gmail.com>
2013-04-06 13:53:36 +0100

    teach brain_from_path to work with yakuba filenames

--- R/NameFunctions.R
+++ R/NameFunctions.R
@@ -5,7 +5,7 @@ template_from_path<-function(f){
 brain_from_path<-function(path){
 	file=basename(path)
 	# first remove template prefix (if it exists)
-	trimmedfile=sub("^[A-Za-z]{2,6}[-0-9]{1,2}_","",file)
+	trimmedfile=sub("^[A-Za-z]{2,6}([-0-9]{1,2}|IS)_","",file)
 	# now go up to first -
 	sub("^([^-]+?)(-|stitched).*","\\1",trimmedfile)
 }
